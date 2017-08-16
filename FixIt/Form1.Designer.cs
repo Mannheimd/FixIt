5a6e6ee566a2440f61f0f4bea8ac6098ded6dbd2
EvilBlueSmartie <31056731+EvilBlueSmartie@users.noreply.github.com>
2017-08-16 22:57:47 +0100

    Original Code

---
+++ FixIt/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What\'s the problem?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Awaiting details...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 253);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(261, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(11, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 75);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Fix It!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hurt feelings",
+            "Dry spell",
+            "Ex wife"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(261, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(11, 83);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(260, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "How much shit are you in?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 288);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fix It";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FixIt/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        // Assign variables.
+        public string selectedItem = "";
+        public bool fail = false;
+        public int progress = 0;
+        public int delay = 100;
+        public int slider = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset progress bar to 0.
+            progressBar1.Value = 0;
+
+            // Retrieve ComboBox selection and assign to string.
+            selectedItem = comboBox1.Text;
+
+            // Retrieve value from Shitometer (TM) and modify delay time for progress bar.
+            slider = trackBar1.Value;
+            delay = delay * slider;
+
+            // Asssign strings to status label depending on input from ComboBox.
+            // Further inputs & outputs can be provided by duplicating these statements.
+            
+            /*
+            switch (comboBox1.Text)
+            {
+                case "Hurt feelings":
+                    statusLabel = "Giving hugs...";
+                    break;
+                case "Dry spell":
+                    statusLabel = "Assuming the position...";
+                    break;
+                case "Ex wife":
+                    statusLabel = "Arranging an 'accident'...";
+                    break;
+                default:
+                    fail = true;
+                    break;
+            }
+            */
+
+            if (selectedItem == "Hurt feelings") { label2.Text = "Giving hugs..."; }
+            if (selectedItem == "Dry spell") { label2.Text = "Assuming position..."; }
+            if (selectedItem == "Ex wife") { label2.Text = "Arranging 'accident'..."; }
+                //if (selectedItem == "") { label2.Text = "..."; }
+
+            // Switches boolean if no item is selected.
+            if (selectedItem == "")
+            {
+                fail = true;
+            }
+            
+            // Request a selection be made if no selection has been input in ComboBox.
+            if (fail == true)
+            {
+                label2.Text = "Please provide an issue to fix.";
+            }
+            // Assuming input is provided, commence progress bar and update status label.
+            else
+            {
+                for (progress = 0; progress < 100; progress = progress + 1)
+                {
+                    progressBar1.PerformStep();
+                    Thread.Sleep(delay);
+                    progressBar1.Value = progress;
+                }
+                if (progress == 100)
+                {
+                    // Inform user of successful issue fix.
+                    label2.Text = "Fixed it!";
+                }
+            }
+        }
+    }
+}
